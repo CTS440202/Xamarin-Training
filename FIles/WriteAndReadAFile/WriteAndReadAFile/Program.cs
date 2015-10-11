4c38f0f16c606584d5995a675e2e0523a19eb326
CTS440202 <bhoosarapu.abhishek@cognizant.com>
2015-10-11 21:20:15 +0530

    Encryption and Decryption

--- FIles/WriteAndReadAFile/WriteAndReadAFile/Program.cs
+++ FIles/WriteAndReadAFile/WriteAndReadAFile/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Web;
 using System.IO;
-
 
 namespace WriteAndReadAFile
 {
@@ -18,13 +16,37 @@ namespace WriteAndReadAFile
 			CreateDirectory.CeateADirectoryAndSubDirectories ();
 			CopyContentFromOneFileToOther.CopyContentFormAndTo(@"/Users/abhishek/GitHub/Xamarin-Training/FIles/WriteAndReadAFile/SimpleText.txt",@"/Users/abhishek/GitHub/Xamarin-Training/FIles/WriteAndReadAFile/SimpleTextCopy.txt");
 
-			Company company = Company.GetData ();
+//			Company company = Company.GetData ();
 
 			// Pass "company" object for conversion object to JSON string
-			string json = new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(company);
+//			string json = new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(company);
+//
+//			// Write that JSON to txt file
+//			File.WriteAllText(Environment.CurrentDirectory + @"\JSON.txt", json);
 
-			// Write that JSON to txt file
-			File.WriteAllText(Environment.CurrentDirectory + @"\JSON.txt", json);
+
+			//**** System.Web.Script.Serialization namespace is not found.
+			Console.WriteLine("---------------------------------------------------\n\n\n");
+			Console.WriteLine ("Enter any Text to Encrypt: ");
+			string dataToEncrypt = Console.ReadLine ();
+			Console.WriteLine ();
+
+			Console.WriteLine ("Enter a password: ");
+			string passwordToEncrypt = Console.ReadLine ();
+			Console.WriteLine ();
+
+			Console.WriteLine ("Please wait White the data is Encrypting...");
+
+			string encryptedData = EncryptAndDecrypt.Encrypt (dataToEncrypt, passwordToEncrypt);
+
+			Console.WriteLine ("Time to retrieve your");
+			Console.WriteLine ("Please enter password to decrypt the String you have entered");
+
+			string passwordToDecrypt = Console.ReadLine (); 
+
+			string decryptedData = EncryptAndDecrypt.Decrypt(encryptedData,passwordToDecrypt);
+
+			Console.WriteLine("Decrypted Data:   " + decryptedData);
 		}
 
 	}
