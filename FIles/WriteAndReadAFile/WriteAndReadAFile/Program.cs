1cbb75e5069229eded9d34ef4c9f64f9532b30cd
CTS440202 <bhoosarapu.abhishek@cognizant.com>
2015-10-11 20:16:07 +0530

    File Folder Update

--- FIles/WriteAndReadAFile/WriteAndReadAFile/Program.cs
+++ FIles/WriteAndReadAFile/WriteAndReadAFile/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Web;
+using System.IO;
+
 
 namespace WriteAndReadAFile
 {
@@ -12,8 +15,16 @@ namespace WriteAndReadAFile
 			//1* Write a program that reads a file and writes out a new file with the lines in reversed order (i.e. the first line in the old file becomes the last one in the new file.)
 			ReverseLineRead.ReadingInReverse(@"/Users/abhishek/GitHub/Xamarin-Training/FIles/WriteAndReadAFile/SimpleText.txt");
 
+			CreateDirectory.CeateADirectoryAndSubDirectories ();
+			CopyContentFromOneFileToOther.CopyContentFormAndTo(@"/Users/abhishek/GitHub/Xamarin-Training/FIles/WriteAndReadAFile/SimpleText.txt",@"/Users/abhishek/GitHub/Xamarin-Training/FIles/WriteAndReadAFile/SimpleTextCopy.txt");
 
+			Company company = Company.GetData ();
 
+			// Pass "company" object for conversion object to JSON string
+			string json = new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(company);
+
+			// Write that JSON to txt file
+			File.WriteAllText(Environment.CurrentDirectory + @"\JSON.txt", json);
 		}
 
 	}
