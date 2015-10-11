82bf8e3374b3948083fe9bbd8c5b171e27f77a8b
CTS440202 <bhoosarapu.abhishek@cognizant.com>
2015-10-11 18:00:24 +0530

    Files

---
+++ FIles/WriteAndReadAFile/WriteAndReadAFile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WriteAndReadAFile
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+//			CreateWriteAndReadFromAFile.WriteToAFile (@"/Users/abhishek/Desktop/abhi.txt");
+//			CreateWriteAndReadFromAFile.ReadFromAFile (@"/Users/abhishek/Desktop/abhi.txt");
+
+			//1* Write a program that reads a file and writes out a new file with the lines in reversed order (i.e. the first line in the old file becomes the last one in the new file.)
+			ReverseLineRead.ReadingInReverse(@"/Users/abhishek/GitHub/Xamarin-Training/FIles/WriteAndReadAFile/SimpleText.txt");
+
+
+
+		}
+
+	}
+}
